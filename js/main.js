3ebf36f7157bf83797f8b53ff64cae60a6a51c47
Loren Hale <loren.hale@gmail.com>
2014-07-09 12:02:44 -0500

    add music videos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
+(function(){
+  $(document).on('click', 'button', function(e){
+    e.preventDefault();
+    var title = $('#title').val();
+    var youtubeId = $('#youtubeId').val();
+    var videoTemplate = $('#templates #video-list-item').html();
+    var renderedTemplate = Robin.render(videoTemplate, {title: title, youtubeId: youtubeId});
+    $('#video-list').append(renderedTemplate);
+    $('#title').val('');
+    $('#youtubeId').val('');
+  });
 
-var videos = [
-  { title: 'Get Krazy', youtubeId: 'GyR2HJ9B7aM' },
-  { title: 'Sadness', youtubeId: 'sIeJSSjTG3k' }
-];
+  var videos = [
+    { title: 'Get Krazy', youtubeId: 'GyR2HJ9B7aM' },
+    { title: 'Sadness', youtubeId: 'sIeJSSjTG3k' }
+  ];
 
-var renderVideoList = function () {
-  // TODO
-};
+  var renderVideoList = function () {
+    var videoTemplate = $('#templates #video-list-item').html();
+    for(var i = 0; i < videos.length; i++) {
+      var renderedTemplate = Robin.render(videoTemplate, videos[i]);
+      $('#video-list').append(renderedTemplate);
+    }
+  };
+
+  renderVideoList();
+})();
