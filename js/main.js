8a326910d9c100368ae6a5702f07949fccecfdaf
Loren Hale <loren.hale@gmail.com>
2014-07-09 11:29:38 -0500

    add a template

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-alert('hi');
+(function() {
+  $(document).on('click', 'button', function(e){
+    e.preventDefault();
+    var name = $('#name').val();
+    var price = $('#price').val();
+    var itemTemplate = $('#templates #item').html();
+    var renderedTemplate = Robin.render(itemTemplate, {name: name, price: price});
+    $('#store').append(renderedTemplate);
+    $('#name').val('');
+    $('#price').val('');
+  });
+})();
