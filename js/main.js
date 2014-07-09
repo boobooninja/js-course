f683677d7eaadc6974979608a7c1caf0d3135c2a
Loren Hale <loren.hale@gmail.com>
2014-07-09 11:07:25 -0500

    add new items

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-alert('hi');
+var itemTemplate = $('#templates .item').html();
+
+var newItemHtml = Robin.render(itemTemplate, {name: 'Dewberry', price: 0.15});
+$('#store').append(newItemHtml);
+
+var items = [
+  { name: 'Coffee', price: 2.25 },
+  { name: 'Juice', price: 3.50 },
+  { name: 'Tea', price: 1.80 },
+];
+
+for (var i = 0; i < items.length; i++) {
+  var newItemHtml = Robin.render(itemTemplate, items[i]);
+  $('#store').append(newItemHtml);
+}
